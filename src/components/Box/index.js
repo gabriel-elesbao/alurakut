5b467b285fefa0a781d7e240247a112f8b52d6ea
gabriel-elesbao <gabriel_elesbao1@hotmail.com>
2021-07-12 20:00:54 -0300

    Dia 1

---
+++ src/components/Box/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  background: #FFFFFF;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 10px;
+
+  /* Css pré pronto */
+
+`
+
+export default Box;

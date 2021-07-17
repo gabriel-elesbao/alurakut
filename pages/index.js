16a8f2360a8365c542b1ff54fce80a9d2bc05dfd
gabriel-elesbao <gabriel_elesbao1@hotmail.com>
2021-07-16 21:21:43 -0300

    Dia 5 - validação

--- pages/index.js
+++ pages/index.js
@@ -237,12 +237,28 @@ export default function Home(props) {
 
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context)
-  const token = cookies.USER_TOKEN
-  const {githubUser} = jwt.decode(token)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+        Authorization: token
+      }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
   return {
     props: {
       githubUser
     }, // will be passed to the page component as props
   }
 }
- 
+
